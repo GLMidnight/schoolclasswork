0cd251147e7c37bb596c794f009f9c36b7e32558
Midnight <Midnight@fmail.co.uk>
2024-10-16 21:45:15 +0100

    Add files via upload

---
+++ python/Python work 02.03.2022/coconut.py
@@ -0,0 +1,4 @@
+coconut = 0
+while coconut < 3:
+    print("clip clop")
+    coconut - coconut + 1
---
+++ python/Python work 02.03.2022/troll.py
@@ -0,0 +1,6 @@
+troll = 0
+while troll <1:
+    print("lol")
+    troll = troll + 1
+    break
+print("phew ")
